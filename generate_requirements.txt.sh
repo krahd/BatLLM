98f41ae14421799e332ac1283892394e659b668e
krahd <tomas@laurenzo.net>
2025-08-06 04:03:10 -0300

    pipreqs to gen requirements.txt

---
+++ generate_requirements.txt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python3 -m  pipreqs.pipreqs --force --ignore .git,tests,docs  --use-local ./src > /dev/null 2>&1
+mv ./src/requirements.txt .
+echo "Requirements.txt:"
+echo ""
+cat requirements.txt
+echo ""
